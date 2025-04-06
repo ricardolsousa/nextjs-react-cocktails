534bff582198d5cbce63bf77954c0d284efc8283
ricardolsousa <ricardolsousa2909@gmail.com>
2025-04-07 00:40:28 +0100

    fix: fixed cocktail skeleton component styles

--- src/components/cocktails/cocktail-skeleton/cocktail-skeleton.tsx
+++ src/components/cocktails/cocktail-skeleton/cocktail-skeleton.tsx
@@ -1,6 +1,6 @@
 const CocktailSkeleton = () => {
   return (
-    <div className="animate-pulse grid grid-cols-2 md:grid-cols-4 gap-4 w-full mt-4">
+    <div className="animate-pulse grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
       {Array.from({ length: 8 }).map((_, i) => (
         <div
           key={i}
--- src/components/search/search-list/search-list.tsx
+++ src/components/search/search-list/search-list.tsx
@@ -5,7 +5,7 @@ interface SearchListProps {
 
 const SearchList = ({ label, children }: SearchListProps) => {
   return (
-    <div className="w-full flex flex-col gap-4">
+    <div className="w-full flex flex-col gap-[32px]">
       <span className="text-white text-3xl font-bold">{label}</span>
       <div className="w-full grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
         {children}
